4df1c7335501d7e9a5d750b35c6ea65beba81117
siyuanchen2020 <63905234+siyuanchen2020@users.noreply.github.com>
2022-03-18 19:08:44 +0100

    standardize the reward

--- gym_basic/envs/basic_env.py
+++ gym_basic/envs/basic_env.py
@@ -19,7 +19,6 @@ class BasicEnv(gym.Env):
         # if we took an action, we were in state 1
         state = 0
 
-        #list_var = []
         '''
         for a in range(1,4):
             for b in range(1,4):
@@ -42,24 +41,8 @@ class BasicEnv(gym.Env):
                                     reward = (200 * throughput * 720) - 25000 * (input_var[0] + 1 + 1 + 1) - 1000 * (
                                                 1 + 1 + 1)
         '''
-        '''
-        for a in range(1, 4):
-            for b in range(1, 4):
-                for c in range(1, 4):
-                    input_var = numpy.array([a, b, c, 1, 1, 1, 1])
-                    # list_var.append(input_var)
-                    numpy.savetxt('input.txt', input_var)
 
-                    os.system(
-                        "xsim-runner.exe --model LawMcComasMOPs.xml --input input.txt --output_txt output_Law.txt")
 
-                    with open('output_Law.txt') as my_file:
-                        # Throughput, Work-In-Process, Parts-Produced, and Lead-Time
-                        output_array = my_file.readlines()
-                    # get the throughput
-                    parts_produced = float(output_array[2])
-                    reward = (200 * parts_produced) - 25000 * (input_var[0] + input_var[1] + input_var[2] + 1) - 1000 * (1 + 1 + 1)
-        '''
         a = randint(1,3)
         b = randint(1,3)
         c = randint(1,3)
@@ -79,7 +62,11 @@ class BasicEnv(gym.Env):
             output_array = my_file.readlines()
         # get the throughput
         parts_produced = float(output_array[2])
-        reward = (200 * parts_produced) - 25000 * (input_var[0] + input_var[1] + input_var[2] + 1) - 1000 * (1 + 1 + 1)
+        reward = (200 * parts_produced) - 25000 * (input_var[0] + input_var[1] + input_var[2] + input_var[3]) - 1000 * (input_var[4] + input_var[5] + input_var[6])
+        
+        print(input_var)
+        #standardize the reward
+        reward = (reward - 300000)/300000
 
 
         state = 1
