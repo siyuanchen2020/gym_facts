d7649f396ad47ecbd62949627596753c8dfac1e9
siyuanchen2020 <63905234+siyuanchen2020@users.noreply.github.com>
2022-03-23 17:44:56 +0100

    new progress

--- gym_basic/envs/basic_env.py
+++ gym_basic/envs/basic_env.py
@@ -6,6 +6,18 @@ import os
 from random import randint
 from stable_baselines3.common.env_checker import check_env
 
+i = 0
+dic = {}
+for a in range(1, 4):
+    for b in range(1, 4):
+        for c in range(1, 4):
+            for d in range(1, 4):
+                for e in range(1, 11):
+                    for f in range(1, 11):
+                        for g in range(1, 11):
+                            i += 1
+                            dic[i] = numpy.array([a, b, c, d, e, f, g])
+
 
 
 class BasicEnv(gym.Env):
@@ -13,36 +25,14 @@ class BasicEnv(gym.Env):
 
     def __init__(self):
         self.action_space = spaces.Discrete(81000)
-        self.observation_space = spaces.Discrete(2)
+        self.observation_space = spaces.Discrete(10000)
 
     def step(self, action):
         # if we took an action, we were in state 1
         state = 0
 
-        '''
-        for a in range(1,4):
-            for b in range(1,4):
-                for c in range(1,4):
-                    for d in range(1,4):
-                        for e in range(1,11):
-                            for f in range(1,11):
-                                for g in range(1,11):
-                                    input_var = numpy.array([a,b,c,d,e,f,g])
-                                    #list_var.append(input_var)
-                                    numpy.savetxt('input.txt', input_var)
-
-                                    os.system("xsim-runner.exe --model LawMcComasMOPs.xml --input input.txt --output_txt output_Law.txt")
-
-                                    with open('output_Law.txt') as my_file:
-                                        # Throughput, Work-In-Process, Parts-Produced, and Lead-Time
-                                        output_array = my_file.readlines()
-                                    # get the throughput
-                                    throughput = float(output_array[0])
-                                    reward = (200 * throughput * 720) - 25000 * (input_var[0] + 1 + 1 + 1) - 1000 * (
-                                                1 + 1 + 1)
-        '''
-
-
+        """
+        #随机初始值
         a = randint(1,3)
         b = randint(1,3)
         c = randint(1,3)
@@ -67,7 +57,79 @@ class BasicEnv(gym.Env):
         print(input_var)
         #standardize the reward
         reward = (reward - 300000)/300000
+        """
 
+        '''
+        if action == 1:
+            input_var = numpy.array([1, 1, 2, 1, 7, 1, 1])
+            numpy.savetxt('input.txt', input_var)
+
+            os.system("xsim-runner.exe --model LawMcComasMOPs.xml --input input.txt --output_txt output_Law.txt")
+
+            with open('output_Law.txt') as my_file:
+                # Throughput, Work-In-Process, Parts-Produced, and Lead-Time
+                output_array = my_file.readlines()
+            # get the parts produced
+            parts_produced = float(output_array[2])
+            reward = (200 * parts_produced) - 25000 * (
+                        input_var[0] + input_var[1] + input_var[2] + input_var[3]) - 1000 * (
+                             input_var[4] + input_var[5] + input_var[6])
+            print(input_var)
+            # standardize the reward
+            reward = (reward - 300000) / 300000
+        elif action == 2:
+            input_var = numpy.array([3, 3, 2, 2, 7, 7, 4])
+            numpy.savetxt('input.txt', input_var)
+
+            os.system("xsim-runner.exe --model LawMcComasMOPs.xml --input input.txt --output_txt output_Law.txt")
+
+            with open('output_Law.txt') as my_file:
+                # Throughput, Work-In-Process, Parts-Produced, and Lead-Time
+                output_array = my_file.readlines()
+            # get the parts produced
+            parts_produced = float(output_array[2])
+            reward = (200 * parts_produced) - 25000 * (
+                        input_var[0] + input_var[1] + input_var[2] + input_var[3]) - 1000 * (
+                             input_var[4] + input_var[5] + input_var[6])
+            print(input_var)
+            # standardize the reward
+            reward = (reward - 300000) / 300000
+        else:
+            input_var = numpy.array([3, 1, 1, 1, 1, 1, 1])
+            numpy.savetxt('input.txt', input_var)
+
+            os.system("xsim-runner.exe --model LawMcComasMOPs.xml --input input.txt --output_txt output_Law.txt")
+
+            with open('output_Law.txt') as my_file:
+                # Throughput, Work-In-Process, Parts-Produced, and Lead-Time
+                output_array = my_file.readlines()
+            # get the parts produced
+            parts_produced = float(output_array[2])
+            reward = (200 * parts_produced) - 25000 * (
+                        input_var[0] + input_var[1] + input_var[2] + input_var[3]) - 1000 * (
+                             input_var[4] + input_var[5] + input_var[6])
+            print(input_var)
+            # standardize the reward
+            reward = (reward - 300000) / 300000
+        '''
+        for i in range(1,81001):
+            if action == i:
+                input_var = dic[i]
+                numpy.savetxt('input.txt', input_var)
+
+                os.system("xsim-runner.exe --model LawMcComasMOPs.xml --input input.txt --output_txt output_Law.txt")
+
+                with open('output_Law.txt') as my_file:
+                    # Throughput, Work-In-Process, Parts-Produced, and Lead-Time
+                    output_array = my_file.readlines()
+                # get the parts produced
+                parts_produced = float(output_array[2])
+                reward = (200 * parts_produced) - 25000 * (
+                        input_var[0] + input_var[1] + input_var[2] + input_var[3]) - 1000 * (
+                             input_var[4] + input_var[5] + input_var[6])
+                print(input_var)
+                # standardize the reward
+                reward = (reward - 250000) / 250000
 
         state = 1
         # regardless of the action, game is done after a single step
@@ -87,5 +149,4 @@ class BasicEnv(gym.Env):
     def close(self):
         pass
 
-
-#check_env(BasicEnv,warn= True, skip_render_check=True)
+# check_env(BasicEnv,warn= True, skip_render_check=True)
---
+++ test2.py
@@ -0,0 +1,80 @@
+import os
+import numpy as np
+import gym
+import random
+import stable_baselines3
+from stable_baselines3 import DQN
+import matplotlib.pyplot as plt
+from stable_baselines3.common.monitor import Monitor
+from stable_baselines3.common import results_plotter
+from stable_baselines3.common.results_plotter import load_results, ts2xy, plot_results
+from stable_baselines3.common.evaluation import evaluate_policy
+from stable_baselines3.common.env_checker import check_env
+from stable_baselines3.common.callbacks import BaseCallback
+
+
+class SaveOnBestTrainingRewardCallback(BaseCallback):
+    """
+    Callback for saving a model (the check is done every ``check_freq`` steps)
+    based on the training reward (in practice, we recommend using ``EvalCallback``).
+
+    :param check_freq:
+    :param log_dir: Path to the folder where the model will be saved.
+      It must contains the file created by the ``Monitor`` wrapper.
+    :param verbose: Verbosity level.
+    """
+    def __init__(self, check_freq: int, log_dir: str, verbose: int = 1):
+        super(SaveOnBestTrainingRewardCallback, self).__init__(verbose)
+        self.check_freq = check_freq
+        self.log_dir = log_dir
+        self.save_path = os.path.join(log_dir, 'best_model')
+        self.best_mean_reward = -np.inf
+
+    def _init_callback(self) -> None:
+        # Create folder if needed
+        if self.save_path is not None:
+            os.makedirs(self.save_path, exist_ok=True)
+
+    def _on_step(self) -> bool:
+        if self.n_calls % self.check_freq == 0:
+
+          # Retrieve training reward
+          x, y = ts2xy(load_results(self.log_dir), 'timesteps')
+          if len(x) > 0:
+              # Mean training reward over the last 100 episodes
+              mean_reward = np.mean(y[-100:])
+              if self.verbose > 0:
+                print(f"Num timesteps: {self.num_timesteps}")
+                print(f"Best mean reward: {self.best_mean_reward:.2f} - Last mean reward per episode: {mean_reward:.2f}")
+
+              # New best model, you could save the agent here
+              if mean_reward > self.best_mean_reward:
+                  self.best_mean_reward = mean_reward
+                  # Example for saving best model
+                  if self.verbose > 0:
+                    print(f"Saving new best model to {self.save_path}")
+                  self.model.save(self.save_path)
+
+        return True
+
+
+log_dir = "/tmp/gym/"
+if not os.path.exists(log_dir):
+    os.makedirs(log_dir)
+env = gym.make("gym_basic:basic-v0")
+env = Monitor(env, log_dir)
+
+# generate the model by DQN
+model = DQN("MlpPolicy", env, verbose=1)
+
+#callback
+callback = SaveOnBestTrainingRewardCallback(check_freq=100, log_dir=log_dir)
+
+model.learn(total_timesteps=1000, callback = callback)
+# save the model
+model.save("dqn_facts")
+
+#plot the result
+timesteps = 1000
+plot_results([log_dir], timesteps, results_plotter.X_TIMESTEPS, "gym_basic:basic-v0")
+plt.show()
